2c104aea51ebe254798804eed40dd2b3fc7a8ecf
Ant-Pavel <artem.pavel2016@yandex.ru>
2017-06-23 22:03:51 +0200

    gruntfile num2

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
               "last 2 versions"
             ]}),
             require("css-mqpacker")({
-              sort: false })
+              sort: true })
           ]
         },
         src: "build/css/*.css"
